9b558b78ea104eafbc047d88ccd832dee43ad402
Nishat Sultana <nishatsultananiha@gmail.com>
2026-09-22 08:56:05 +0600

    Configure Aiven MySQL connection

--- Backend/db.js
+++ Backend/db.js
@@ -7,24 +7,20 @@ require("dotenv").config();
 ========================= */
 
 const db = mysql.createPool({
-
     host: process.env.DB_HOST,
-
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
-
     password: process.env.DB_PASSWORD,
-
     database: process.env.DB_NAME,
 
-    /* UTF-8 support for emojis */
+    ssl: {
+        rejectUnauthorized: false
+    },
+
     charset: "utf8mb4",
-
     waitForConnections: true,
-
     connectionLimit: 10,
-
     queueLimit: 0
-
 });
 
 
